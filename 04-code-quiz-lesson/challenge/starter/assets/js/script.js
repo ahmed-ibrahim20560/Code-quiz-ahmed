b114ac2f6e91da3692faf0d92383640c9e0f84e2
ahmed-ibrahim20560 <ahmedibrahim20560@gmail.com>
2024-01-02 16:31:41 +0000

    added sample questions in the form of an object (from copy code quiz folder)

--- 04-code-quiz-lesson/challenge/starter/assets/js/script.js
+++ 04-code-quiz-lesson/challenge/starter/assets/js/script.js
@@ -9,3 +9,31 @@ const initials = document.getElementById("initals")
 const submitBtn = document.getElementById("submit")
 const feedbackEl = document.getElementById("feedback") 
 
+let questions = [ 
+  {
+    title: "First Question",
+    choices: "ChoiceA, ChoiceB, ChoiceC",
+    answer: "ChoiceB",
+  },
+  {
+    title: "First Question",
+    choices: "ChoiceA, ChoiceB, ChoiceC",
+    answer: "ChoiceB",
+  },
+  {
+    title: "First Question",
+    choices: "ChoiceA, ChoiceB, ChoiceC",
+    answer: "ChoiceB",
+  },
+  {
+    title: "First Question",
+    choices: "ChoiceA, ChoiceB, ChoiceC",
+    answer: "ChoiceB",
+  },
+  {
+    title: "First Question",
+    choices: "ChoiceA, ChoiceB, ChoiceC",
+    answer: "ChoiceB",
+  }
+]
+
