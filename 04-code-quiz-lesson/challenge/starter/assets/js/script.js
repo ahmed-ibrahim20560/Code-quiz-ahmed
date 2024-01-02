33a8a7b3a89ebe8a9de654c85ebaf6c7d957e814
ahmed-ibrahim20560 <ahmedibrahim20560@gmail.com>
2024-01-02 16:28:30 +0000

    added a new folder for the js and created a list of needed variables that link to ids from the html (from copy code quiz folder)

---
+++ 04-code-quiz-lesson/challenge/starter/assets/js/script.js
@@ -0,0 +1,11 @@
+let timerEl = document.getElementById("time")
+let startScreen = documen.getElementById("start-screen")
+const startBtn = document.getElementById("start")
+const questionTitle = document.getElementById("question-title")
+const questionChoices = document.getElementById("choices")
+const endScreen = document.getElementById("end-screen")
+const finalScore = document.getElementById("final-score")
+const initials = document.getElementById("initals")
+const submitBtn = document.getElementById("submit")
+const feedbackEl = document.getElementById("feedback") 
+
